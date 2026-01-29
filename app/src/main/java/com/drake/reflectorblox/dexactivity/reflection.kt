a634fe138dacc08411e9b7231485585a5a2679ec
ekard <105611677+helloplauz10@users.noreply.github.com>
2026-01-29 22:09:18 +0800

    throw exceptions

--- app/src/main/java/com/drake/reflectorblox/dexactivity/reflection.kt
+++ app/src/main/java/com/drake/reflectorblox/dexactivity/reflection.kt
@@ -3,28 +3,20 @@ package com.drake.reflectorblox.dexactivity
 import java.lang.reflect.Field
 import java.lang.reflect.Method
 
-fun getField(clazz: Class<*>, name: String): Field? {
-    try {
-        clazz.getField(name).let {
-            if (!it.isAccessible) {
-                it.isAccessible = true
-            }
-            return it
+fun getField(clazz: Class<*>, name: String): Field {
+    clazz.getField(name).let {
+        if (!it.isAccessible) {
+            it.isAccessible = true
         }
-    } catch (e: NoSuchFieldException) {
-        return null
+        return it
     }
 }
 
-fun getMethod(clazz: Class<*>, method: String, vararg parameterTypes: Class<*>): Method? {
-    try {
-        clazz.getMethod(method, *parameterTypes).let {
-            if (!it.isAccessible) {
-                it.isAccessible = true
-            }
-            return it
+fun getMethod(clazz: Class<*>, method: String, vararg parameterTypes: Class<*>): Method {
+    clazz.getMethod(method, *parameterTypes).let {
+        if (!it.isAccessible) {
+            it.isAccessible = true
         }
-    } catch (e: NoSuchMethodException) {
-        return null
+        return it
     }
 }
